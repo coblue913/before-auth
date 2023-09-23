87c394111521b15c8c9f5ae71f1a859a87b7e5fe
seattle <coblue913@gmail.com>
2023-09-23 21:48:39 +0900

    feat: 자격 증명 제공자 추가

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,28 @@
+/**
+ * @see https://next-auth.js.org/configuration/initialization#route-handlers-app
+ */
 import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 const handler = NextAuth({
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: '아이디', type: 'text', placeholder: '이메일 주소를 입력해주세요' },
+        password: { label: '비밀번호', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        // TODO: DB 에서 username 을 찾고 비밀번호가 맞는지 체크
+        const user = { id: '1', name: 'tiger', email: 'coblue@gmail.com' };
+
+        if (user) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
 });
 
 // as ?로 export 하는 모듈의 이름을 지정할 수 있음
