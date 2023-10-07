a2bc5abc0b761e70fc56d0f2251b75828e14eb4f
seattle <coblue913@gmail.com>
2023-10-07 20:28:28 +0900

    refactor: 엑세스 토큰 인증 로직 추가

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,9 +1,17 @@
+import { verifyJwt } from '@/app/lib/jwt';
 import prisma from '@/app/lib/prisma';
 
 export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ): Promise<Response> {
+  const accessToken = request.headers.get('authorization');
+  if (!accessToken || !verifyJwt(accessToken)) {
+    return new Response(JSON.stringify({ error: '회원 인증에 실패했습니다.' }), {
+      status: 401,
+    });
+  }
+
   console.log(params);
 
   const id: number = Number(params.id);
